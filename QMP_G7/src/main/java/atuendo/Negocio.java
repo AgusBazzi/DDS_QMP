8ac6438405970f0a50ea1699a386a09395c8a72e
Lucas Schnedier <llschneider2000@gmail.com>
2022-04-27 19:27:27 -0300

    feat<Negocio>: Singleton

--- QMP_G7/src/main/java/atuendo/Negocio.java
+++ QMP_G7/src/main/java/atuendo/Negocio.java
@@ -1,14 +1,28 @@
 package atuendo;
 
-import java.util.ArrayList;
-import java.util.List;
+import prendas.Categoria;
+import prendas.Compatibilidades;
+import prendas.TipoPrenda;
+
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Negocio {
-  private List<Uniforme> uniformes = new ArrayList<Uniforme>();
+  private List<Uniforme> uniformes;
+  private static Negocio instance;
 
   public void agregarUniforme(Uniforme unUniforme) {
     this.uniformes.add(unUniforme);
+  }
+
+  private Negocio(){};
+
+  public static Negocio getInstance() {
+    if(instance == null) {
+      instance = new Negocio();
+      instance.uniformes = new ArrayList<>();
+    }
+    return instance;
   }
 
   public Uniforme getUniforme(String unaInstitucion) throws Exception {
