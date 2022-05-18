c1810c4347312eacf67e245d759e5e1b57cd3bb1
Lucas Schnedier <llschneider2000@gmail.com>
2022-05-18 16:56:48 -0300

    test <TestGuardarropa>: test de creacion de varias sugerencias

--- QMP_G7/src/test/java/TestGuardarropa/TestGuardarropa.java
+++ QMP_G7/src/test/java/TestGuardarropa/TestGuardarropa.java
@@ -2,9 +2,12 @@ package TestGuardarropa;
 
 import atuendo.Atuendo;
 import atuendo.Guardarropa;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import prendas.*;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -62,11 +65,27 @@ public class TestGuardarropa {
     this.assertCategoriaUnica(unAtuendoSugerido, Categoria.CALZADO);
     this.assertCategoriaUnica(unAtuendoSugerido, Categoria.ACCESORIO);
   }
+  @Test
+  public void variasSugerenciasSeCreanCorrectamente() {
+    List<Atuendo> variasSugerencias = unGuardarropa.variasSugerencias(3);
+    assertEquals(variasSugerencias.size(), 3);
+    variasSugerencias.forEach( atuendo -> { this.assertCategoriaUnica(atuendo, Categoria.PARTE_SUPERIOR);
+                                            this.assertCategoriaUnica(atuendo, Categoria.PARTE_INFERIOR);
+                                            this.assertCategoriaUnica(atuendo, Categoria.CALZADO);
+                                            this.assertAccesorioUnicoOInexistente(atuendo); } );
+  }
 
   private void assertCategoriaUnica(Atuendo unAtuendo, Categoria categoria) {
     assertEquals(unAtuendo.getPrendas()
         .stream()
         .filter(prenda -> prenda.tieneCategoria(categoria))
         .count(), 1);
+  }
+
+  private void assertAccesorioUnicoOInexistente(Atuendo unAtuendo) {
+    Long cantAccesorios = unAtuendo.getPrendas()
+                          .stream()
+                          .filter( prenda -> prenda.tieneCategoria(Categoria.ACCESORIO)).count();
+    assertTrue(cantAccesorios == 0 || cantAccesorios == 1);
   }
 }
